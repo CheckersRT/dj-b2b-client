a04f3206b6901c6c17e7c12780bdc1ca93376945
Tom Dent <tomdent991@hotmail.com>
2024-01-25 17:51:06 +0100

    edit to eq

--- src/components/Mixer/EqKnob/handleEq.js
+++ src/components/Mixer/EqKnob/handleEq.js
@@ -1,25 +1,20 @@
-import {eqCh1, eqCh2} from "../../../utils/audioNodeSetUp"
+import { eqCh1, eqCh2 } from "../../../utils/audioNodeSetUp";
 
 export default function handleEq(rotation, sendReceive, channel, param) {
+  let gainValue;
+  if (rotation >= 0) {
+    const percentage = rotation / 150;
+    gainValue = percentage * 6;
+  } else if (rotation < 0) {
+    const percentage = rotation / 150;
+    gainValue = percentage * 26;
+  }
 
-    let gainValue;
-    if (rotation >= 0) {
-      const percentage = rotation / 150;
-      gainValue = percentage * 6
-    } else if (rotation < 0) {
-      const percentage = rotation / 150;
-      gainValue = percentage * 26;
-      
-    }
-
-
-
-
-    if(channel === 1) {
-        eqCh1[param].value = gainValue
-        console.log(eqCh1[param].value)
-    } else if(channel === 2) {
-        eqCh2[param].value = gainValue
-        console.log(eqCh2[param].value)
-    }
+  if (channel === 1) {
+    eqCh1[param].value = gainValue;
+    console.log(eqCh1[param].value);
+  } else if (channel === 2) {
+    eqCh2[param].value = gainValue;
+    console.log(eqCh2[param].value);
+  }
 }
