c5b466e6ecced9e0a95a174729b2c5dd9de76388
Tom Dent <tomdent991@hotmail.com>
2024-02-19 16:21:03 +0100

    added url config

---
+++ src/api/consts.js
@@ -0,0 +1,8 @@
+const production = {
+  url: "https://dj-b2b-server-production.up.railway.app",
+};
+const development = {
+  url: "http://localhost:3000",
+};
+export const config =
+  process.env.NODE_ENV === "development" ? development : production;
--- src/api/getMetaData.js
+++ src/api/getMetaData.js
@@ -1,8 +1,12 @@
+import {config} from "./consts.js"
+
+const url = config.url
+
 export default async function getMetaData(formData) {
   try {
-    const response = await fetch(
+    const response = await fetch(`${url}/routes/getMetaData`,
       // "http://localhost:3030/routes/getMetaData",
-      "https://dj-b2b-server-production.up.railway.app/routes/getMetaData",
+      // "https://dj-b2b-server-production.up.railway.app/routes/getMetaData",
       {
         method: "POST",
         body: formData,
