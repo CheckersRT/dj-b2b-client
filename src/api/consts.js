86735a169ef5ecacb5301318d0b60273f74a1d43
Tom Dent <tomdent991@hotmail.com>
2024-02-20 10:29:04 +0100

    remove slash in filepath

--- src/api/consts.js
+++ src/api/consts.js
@@ -2,7 +2,7 @@ const production = {
   url: "https://dj-b2b-server-production.up.railway.app",
 };
 const development = {
-  url: "http://localhost:3000",
+  url: "http://localhost:3030",
 };
 export const config =
   process.env.NODE_ENV === "development" ? development : production;
